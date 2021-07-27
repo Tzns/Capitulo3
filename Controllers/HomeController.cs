46dcfeb53d16a60d391ba20649e7320e0f5287a9
Thiago Calazans <thicalaza@gmail.com>
2021-07-27 16:51:16 -0300

    c

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,9 +16,22 @@ namespace PartyInvites.Controllers
             return View();
         }
 
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(t => t.WillAttend == true));
         }
 
     }
